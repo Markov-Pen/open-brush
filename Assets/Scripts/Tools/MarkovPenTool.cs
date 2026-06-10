d0719e2fb159f2bc71c93e85dc85f3ff17b125fb
Georgy Libov <libov.g@gmail.com>
2026-06-10 14:48:17 +0200

    Refactor: Move up vector code into tool
    Move the controller up vector logic into the Markov pen tool.

--- Assets/Scripts/Tools/MarkovPenTool.cs
+++ Assets/Scripts/Tools/MarkovPenTool.cs
@@ -4,97 +4,82 @@ namespace TiltBrush
 {
     public class MarkovPenTool : BaseTool
     {
-        // The parent of all of our tool's visual indicator objects.
-        private GameObject m_ToolDirectionIndicator;
+        public string TargetName = "monterey_controller_R";
 
-        // Whether this tool should follow the controller or not.
-        private bool m_IsLockedToController;
+        public float Length = 10f;
+        public float Width = 0.01f;
 
-        // The controller that this tool is attached to.
-        private Transform m_BrushController;
-
+        private Transform m_Target;
+        private LineRenderer m_Line;
         // Init is similar to Awake(), and should be used for initializing references and other setup code.
-        public override void Init()
-        {
-            base.Init();
 
-            // Get the visual direction indicator by name, like FlyTool does.
-            m_ToolDirectionIndicator = transform.Find("DirectionIndicator").gameObject;
+
+
+        override protected void Awake()
+        {
+            base.Awake();
+
+            m_Line = gameObject.AddComponent<LineRenderer>();
+            m_Line.positionCount = 2;
+            m_Line.startWidth = Width;
+            m_Line.endWidth = Width;
+            m_Line.useWorldSpace = true;
+            m_Line.material = new Material(Shader.Find("Sprites/Default"));
+
         }
 
-        // What to do when the tool is enabled or disabled.
-        public override void EnableTool(bool isEnabled)
-        {
-            base.EnableTool(isEnabled);
 
-            if (isEnabled)
-            {
-                m_IsLockedToController = m_SketchSurface.IsInFreePaintMode();
-
-                if (m_IsLockedToController)
-                {
-                    m_BrushController = InputManager.m_Instance.GetController(InputManager.ControllerName.Brush);
-                }
-
-            }
-
-            // Make sure our UI reticle isn't active.
-            SketchControlsScript.m_Instance.ForceShowUIReticle(false);
-        }
-
-        // What to do when the tool is hidden / shown.
-        public override void HideTool(bool isHidden)
-        {
-            base.HideTool(isHidden);
-
-            // Show the direction indicator while the tool is visible.
-            m_ToolDirectionIndicator.SetActive(!isHidden);
-        }
-
-        // What to do when all the tools run their update functions.
-        // Note that this is separate from Unity's Update script.
-        // All input handling should be done here.
         public override void UpdateTool()
         {
             base.UpdateTool();
-
-            Transform attachPoint = InputManager.m_Instance.GetBrushControllerAttachPoint();
-            PointerManager.m_Instance.SetMainPointerPosition(attachPoint.position);
-
-            // Keep the tool angle correct.
-            m_ToolDirectionIndicator.transform.localRotation =
-                Quaternion.Euler(PointerManager.m_Instance.FreePaintPointerAngle, 0f, 0f);
-        }
-
-        // The actual Unity update function, used to update transforms and perform per-frame operations.
-        private void Update()
-        {
-            // If we're not locking to a controller, update our transforms now, instead of in LateUpdate.
-            if (!m_IsLockedToController)
+            if (m_Target == null)
             {
-                UpdateTransformsFromControllers();
+                TryFindTarget();
+
+                if (m_Target == null)
+                {
+                    return;
+                }
             }
+
+            DrawUpVector();
         }
 
-        public override void LateUpdateTool()
+        private void TryFindTarget()
         {
-            base.LateUpdateTool();
-            UpdateTransformsFromControllers();
+            GameObject targetObject = GameObject.Find(TargetName);
+
+            if (targetObject == null)
+            {
+                return;
+            }
+
+            m_Target = targetObject.transform;
         }
 
-        private void UpdateTransformsFromControllers()
+        private void DrawUpVector()
         {
-            // Lock tool to camera controller.
-            if (m_IsLockedToController)
-            {
-                transform.position = m_BrushController.position;
-                transform.rotation = m_BrushController.rotation;
-            }
-            else
-            {
-                transform.position = SketchSurfacePanel.m_Instance.transform.position;
-                transform.rotation = SketchSurfacePanel.m_Instance.transform.rotation;
-            }
+            Vector3 startPosition = m_Target.position;
+            Vector3 endPosition = startPosition + m_Target.up * Length;
+
+            m_Line.SetPosition(0, startPosition);
+            m_Line.SetPosition(1, endPosition);
         }
+
+        private string GetPath(Transform targetTransform)
+        {
+            string path = targetTransform.name;
+
+            while (targetTransform.parent != null)
+            {
+                targetTransform = targetTransform.parent;
+                path = targetTransform.name + "/" + path;
+            }
+
+            return path;
+        }
+
+
+
     }
 }
